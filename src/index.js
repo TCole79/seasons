49673f78daf79040aa13d30d413e86862a7c4c87
Tristan Cole <93275422+TCole79@users.noreply.github.com>
2022-11-10 16:31:52 -0700

    Update index.js
    * Change to Class based component which is step one in the solution to using the geolocator

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => console.log(position),
-        (err) => console.log(err)
-    );    
+// functional component is not suitable for async geolocator - have to make it a class based component which is step one towards the solution
+// const App = () => {
+//     window.navigator.geolocation.getCurrentPosition(
+//         (position) => console.log(position),
+//         (err) => console.log(err)
+//     );    
 
-    return <div>Hi there</div>
-};
+//     return <div>Latitude: </div>
+// };
 
-ReactDOM.render (<App />, document.querySelector('#root')
-);
+// class based component of the above
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+        ); 
+        return <div>Latitude: </div>
+    }
+}
+
+ReactDOM.render (<App />, document.querySelector('#root'));
