8b00425525b2cbde3e00156b2b9028b2458a01a9
Tristan Cole <93275422+TCole79@users.noreply.github.com>
2022-11-10 16:32:33 -0700

    Update index.js
    * Remove Functional component version + notes

--- src/index.js
+++ src/index.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// functional component is not suitable for async geolocator - have to make it a class based component which is step one towards the solution
-// const App = () => {
-//     window.navigator.geolocation.getCurrentPosition(
-//         (position) => console.log(position),
-//         (err) => console.log(err)
-//     );    
-
-//     return <div>Latitude: </div>
-// };
-
-// class based component of the above
 class App extends React.Component {
     render() {
         window.navigator.geolocation.getCurrentPosition(
