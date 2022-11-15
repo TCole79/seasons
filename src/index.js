acef6b8fb5088896375ee70772d81dadc8bbc96c
Tristan Cole <93275422+TCole79@users.noreply.github.com>
2022-11-15 13:51:04 -0700

    Update index.js
    * Error handling + add error into state
    * Conditionally rendering content

--- src/index.js
+++ src/index.js
@@ -7,20 +7,28 @@ class App extends React.Component {
         super(props);
 
         // this is the only time we allow direct assignment to this.state
-        this.state = { lat: null };
+        this.state = { lat: null, errorMessage: '' };
 
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 // we called setState!
                 this.setState({ lat: position.coords.latitude });
             },
-            err => console.log(err)
+            err => {
+                this.setState({ errorMessage: err.message });
+            }
         ); 
     }
 
     // React says we must define render! No render = error
     render() {
-        return <div>Latitude: { this.state.lat }</div>
+       if (this.state.errorMessage && !this.state.lat) {
+        return <div>Error: { this.state.errorMessage }</div>;
+       }
+       if (!this.state.errorMessage && this.state.lat) {
+        return <div>Latitude: { this.state.lat }</div>;
+       }
+       return <div>Loading!</div>;
     }
 }
 
