90d218d2b84b35e62c9407d847bd61f8e6a32383
Tristan Cole <93275422+TCole79@users.noreply.github.com>
2022-11-15 13:16:45 -0700

    Update index.js
    * Refactor

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    // constructor is optional for initial setup if needed
     constructor(props) {
         super(props);
 
         // this is the only time we allow direct assignment to this.state
-        this.state = { latitude: null };
+        this.state = { lat: null };
 
         window.navigator.geolocation.getCurrentPosition(
             position => {
                 // we called setState!
-                this.setState({ latitude: position.coords.latitude });
+                this.setState({ lat: position.coords.latitude });
             },
             err => console.log(err)
         ); 
@@ -19,7 +20,7 @@ class App extends React.Component {
 
     // React says we must define render! No render = error
     render() {
-        return <div>Latitude: { this.state.latitude }</div>
+        return <div>Latitude: { this.state.lat }</div>
     }
 }
 
