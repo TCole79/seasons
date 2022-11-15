e1d116781c7ddaf8aa021391b3219ac45815165c
Tristan Cole <93275422+TCole79@users.noreply.github.com>
2022-11-15 12:50:36 -0700

    Update index.js
    * Initialize State using constructors example

--- src/index.js
+++ src/index.js
@@ -2,6 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { latitude: null };
+    }
+
+    // React says we must define render! No render = error
     render() {
         window.navigator.geolocation.getCurrentPosition(
             (position) => console.log(position),
